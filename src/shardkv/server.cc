ee6d6e6c1a0e21beec92112e39bfae9ddc6500b7
Sayed Bilal Bari <sayedbilal.bari@stonybrook.edu>
2023-11-16 17:01:22 +0000

    Corrected loc_id to site_id mapping for ShardKV

--- src/shardkv/server.cc
+++ src/shardkv/server.cc
@@ -20,7 +20,7 @@ int ShardKvServer::Put(const uint64_t& oid,
     std::recursive_mutex my_mutex;
     my_mutex.lock();
     RaftServer& raft_server = GetRaftServer();
-    Log_info("KV Server %lu -> Starting, put for %lu",raft_server.loc_id_,oid);
+    Log_info("KV Server %lu -> Starting, put for %lu",raft_server.site_id_,oid);
     auto s = make_shared<MultiStringMarshallable>();
     s->data_.push_back(to_string(oid));
     s->data_.push_back("put");
@@ -29,28 +29,28 @@ int ShardKvServer::Put(const uint64_t& oid,
     std::shared_ptr<Marshallable> my_command(s);
     uint64_t index = 0,term = 0;
     uint64_t *pointer_to_index = &index, *pointer_to_term = &term;
-    Log_info("KV Server %lu  -> Before checking start, put for %lu",raft_server.loc_id_,oid);
+    Log_info("KV Server %lu  -> Before checking start, put for %lu",raft_server.site_id_,oid);
     my_mutex.unlock();
     if(raft_server.Start(my_command, pointer_to_index, pointer_to_term))
     {
-      Log_info("KV Server %lu  -> Inside start block, put for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> Inside start block, put for %lu",raft_server.site_id_,oid);
       my_mutex.lock();
       auto ev = Reactor::CreateSpEvent<IntEvent>();
       outstanding_requests_[to_string(oid)] = ev;
-      Log_info("KV Server %lu  -> Before wait, put for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> Before wait, put for %lu",raft_server.site_id_,oid);
       my_mutex.unlock();
       ev->Wait(25000000);
       my_mutex.lock();
-      Log_info("KV Server %lu -> After wait, put for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu -> After wait, put for %lu",raft_server.site_id_,oid);
       if(ev->status_ == Event::TIMEOUT)
       {
-        Log_info("KV Server %lu  -> Sending timeout put for %lu",raft_server.loc_id_,oid);
+        Log_info("KV Server %lu  -> Sending timeout put for %lu",raft_server.site_id_,oid);
         my_mutex.unlock();
         return KV_TIMEOUT;
       }
       else
       {
-        Log_info("KV Server %lu -> Sending success put for %lu",raft_server.loc_id_,oid);
+        Log_info("KV Server %lu -> Sending success put for %lu",raft_server.site_id_,oid);
         my_mutex.unlock();
         return KV_SUCCESS;
       }
@@ -58,7 +58,7 @@ int ShardKvServer::Put(const uint64_t& oid,
     else
     {
       my_mutex.lock();
-      Log_info("KV Server %lu -> Sending back no leader, put for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu -> Sending back no leader, put for %lu",raft_server.site_id_,oid);
       my_mutex.unlock();
       return KV_NOTLEADER;
     }
@@ -71,7 +71,7 @@ int ShardKvServer::Append(const uint64_t& oid,
     std::recursive_mutex my_mutex;
     my_mutex.lock();
     RaftServer& raft_server = GetRaftServer();
-    Log_info("KV Server %lu -> Starting, append for %lu",raft_server.loc_id_,oid);
+    Log_info("KV Server %lu -> Starting, append for %lu",raft_server.site_id_,oid);
     auto s = make_shared<MultiStringMarshallable>();
     s->data_.push_back(to_string(oid));
     s->data_.push_back("append");
@@ -80,7 +80,7 @@ int ShardKvServer::Append(const uint64_t& oid,
     std::shared_ptr<Marshallable> my_shared(s);
     uint64_t index = 0,term = 0;
     uint64_t *pointer_to_index = &index, *pointer_to_term = &term;
-    Log_info("KV Server %lu  -> Before calling start, append for %lu",raft_server.loc_id_,oid);
+    Log_info("KV Server %lu  -> Before calling start, append for %lu",raft_server.site_id_,oid);
     my_mutex.unlock();
     if(raft_server.Start(my_shared,pointer_to_index,pointer_to_term))
     {
@@ -88,19 +88,19 @@ int ShardKvServer::Append(const uint64_t& oid,
       auto ev = Reactor::CreateSpEvent<IntEvent>();
       outstanding_requests_[to_string(oid)] = ev;
       my_mutex.unlock();
-      Log_info("KV Server %lu  -> Before wait, append for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> Before wait, append for %lu",raft_server.site_id_,oid);
       ev->Wait(25000000);
       my_mutex.lock();
-      Log_info("KV Server %lu  -> After wait, append for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> After wait, append for %lu",raft_server.site_id_,oid);
       if(ev->status_ == Event::TIMEOUT)
       {
-        Log_info("KV Server %lu  -> Returning timed out,append for %lu",raft_server.loc_id_,oid);
+        Log_info("KV Server %lu  -> Returning timed out,append for %lu",raft_server.site_id_,oid);
         my_mutex.unlock();
         return KV_TIMEOUT;
       }
       else
       {
-        Log_info("KV Server %lu  -> Returning success, append for %lu",raft_server.loc_id_,oid);
+        Log_info("KV Server %lu  -> Returning success, append for %lu",raft_server.site_id_,oid);
         my_mutex.unlock();
         return KV_SUCCESS;
       }
@@ -108,7 +108,7 @@ int ShardKvServer::Append(const uint64_t& oid,
     else
     {
       my_mutex.lock();
-      Log_info("KV Server %lu  -> Returning not leader, append for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> Returning not leader, append for %lu",raft_server.site_id_,oid);
       my_mutex.unlock();
       return KV_NOTLEADER;
     }
@@ -120,13 +120,13 @@ int ShardKvServer::Get(const uint64_t& oid,
     std::recursive_mutex my_mutex;
     my_mutex.lock();
     RaftServer& raft_server = GetRaftServer();
-    Log_info("KV Server %lu  -> Starting get for %lu",raft_server.loc_id_,oid);
+    Log_info("KV Server %lu  -> Starting get for %lu",raft_server.site_id_,oid);
     auto s = make_shared<MultiStringMarshallable>();
     s->data_.push_back(to_string(oid));
     s->data_.push_back("get");
     s->data_.push_back(k);
     s->data_.push_back("get");
-    Log_info("KV Server %lu  -> Marshallable created, get for %lu",raft_server.loc_id_,oid);
+    Log_info("KV Server %lu  -> Marshallable created, get for %lu",raft_server.site_id_,oid);
     std::shared_ptr<Marshallable> my_shared(s);
     uint64_t index = 0,term = 0;
     uint64_t *pointer_to_index = &index, *pointer_to_term = &term;
@@ -134,23 +134,23 @@ int ShardKvServer::Get(const uint64_t& oid,
     if(raft_server.Start(my_shared,pointer_to_index,pointer_to_term))
     {
       my_mutex.lock();
-      Log_info("KV Server %lu  -> Inside start, get for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> Inside start, get for %lu",raft_server.site_id_,oid);
       auto ev = Reactor::CreateSpEvent<IntEvent>();
       outstanding_requests_[to_string(oid)] = ev;
       my_mutex.unlock();
-      Log_info("KV Server %lu  -> Before wait, get for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> Before wait, get for %lu",raft_server.site_id_,oid);
       ev->Wait(25000000);
       my_mutex.lock();
-      Log_info("KV Server %lu  -> After wait, get for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> After wait, get for %lu",raft_server.site_id_,oid);
       if(ev->status_ == Event::TIMEOUT)
       {
-        Log_info("KV Server %lu  -> Sending time out, get for %lu",raft_server.loc_id_,oid);
+        Log_info("KV Server %lu  -> Sending time out, get for %lu",raft_server.site_id_,oid);
         my_mutex.unlock();
         return KV_TIMEOUT;
       }
       else
       {
-        Log_info("KV Server %lu  -> Sending success response, get for %lu",raft_server.loc_id_,oid);
+        Log_info("KV Server %lu  -> Sending success response, get for %lu",raft_server.site_id_,oid);
         if(kv_store_.find(k)!=kv_store_.end())
           (*v) = kv_store_[k];
         my_mutex.unlock();
@@ -159,7 +159,7 @@ int ShardKvServer::Get(const uint64_t& oid,
     }
     else
     {
-      Log_info("KV Server %lu  -> Sending not leader, get for %lu",raft_server.loc_id_,oid);
+      Log_info("KV Server %lu  -> Sending not leader, get for %lu",raft_server.site_id_,oid);
       return KV_NOTLEADER;
     }
 }
@@ -169,22 +169,22 @@ void ShardKvServer::OnNextCommand(Marshallable& m) {
     std::recursive_mutex my_mutex;
     my_mutex.lock();
     RaftServer& raft_server = GetRaftServer();
-    Log_info("KV Server %lu  -> Inside OnNextCommand",raft_server.loc_id_);
+    Log_info("KV Server %lu  -> Inside OnNextCommand",raft_server.site_id_);
     auto v = (MultiStringMarshallable*)(&m);
     string oid = v->data_[0];
     uint64_t oid_int = stoull(oid);
     string command = v->data_[1];
     string key = v->data_[2];
     string value = v->data_[3];
-    Log_info("KV Server %lu  -> OnNext, processing request %lu",raft_server.loc_id_,oid_int);
+    Log_info("KV Server %lu  -> OnNext, processing request %lu",raft_server.site_id_,oid_int);
     if(command == "put")
     {
       kv_store_[key] = value;
-      Log_info("KV Server %lu  -> OnNext, Got put command for %lu",raft_server.loc_id_,oid_int);
+      Log_info("KV Server %lu  -> OnNext, Got put command for %lu",raft_server.site_id_,oid_int);
     }
     else if(command == "append")
     {
-      Log_info("KV Server %lu  -> OnNext, Got append command for %lu",raft_server.loc_id_,oid_int);
+      Log_info("KV Server %lu  -> OnNext, Got append command for %lu",raft_server.site_id_,oid_int);
       if(kv_store_.find(key)!=kv_store_.end())
       {
         kv_store_[key] = kv_store_[key]+value;
@@ -193,13 +193,13 @@ void ShardKvServer::OnNextCommand(Marshallable& m) {
         kv_store_[key] = value;
     }
     else
-      Log_info("KV Server %lu  -> OnNext, Got get command for %lu",raft_server.loc_id_,oid_int);
+      Log_info("KV Server %lu  -> OnNext, Got get command for %lu",raft_server.site_id_,oid_int);
     if(raft_server.state=="leader" && outstanding_requests_.find(oid)!=outstanding_requests_.end())
     {
       if(outstanding_requests_[oid]->status_ != Event::TIMEOUT)
         outstanding_requests_[oid]->Set(1);
       outstanding_requests_.erase(oid);
-      Log_info("KV Server %lu -> OnNext, Processed and deleted pending request %lu",raft_server.loc_id_,oid_int);
+      Log_info("KV Server %lu -> OnNext, Processed and deleted pending request %lu",raft_server.site_id_,oid_int);
     }
     my_mutex.unlock();
 }
